682bb2165a6e0b4efdb693f8710a6f1b990e782f
Mickael Silva <mickael.silva@corenet.com.br>
2023-07-31 17:32:43 -0400

    Layout tela inicial

--- src/app/modules/access/initial/initial.component.ts
+++ src/app/modules/access/initial/initial.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CoreModule } from 'src/app/shared/core.module';
 
 
 @Component({
--- src/app/shared/core.module.ts
+++ src/app/shared/core.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 
-
 @NgModule({
   declarations: [],
   imports: [
