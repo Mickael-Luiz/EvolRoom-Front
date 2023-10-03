fbd431bde98a3b14ca193bee5f2144a36fc71d00
Mickael Dias <mickaeldias00@gmail.com>
2023-10-03 18:51:07 -0400

    create modules

---
+++ src/app/modules/edugames/edugames.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EdugamesRoutingModule } from './edugames-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EdugamesRoutingModule
+  ]
+})
+export class EdugamesModule { }
