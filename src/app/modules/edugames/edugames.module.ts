c174ac75c5facb6905a3f1aae343210e9ce7d22a
Mickael Dias <mickaeldias00@gmail.com>
2023-10-09 10:48:38 -0400

    ajuste modulo edugames

--- src/app/modules/edugames/edugames.module.ts
+++ src/app/modules/edugames/edugames.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { EdugamesRoutingModule } from './edugames-routing.module';
 import { EdugamesHomeComponent } from './edugames-home/edugames-home.component';
-import { FinancialHomeComponent } from './financial-home/financial-home.component';
 
 
 @NgModule({
   declarations: [
     EdugamesHomeComponent,
-    FinancialHomeComponent
   ],
   imports: [
     CommonModule,
